bef735d4fd969d27ebed5207c68fe69af0f38528
Wonseok Choi <cerdure@gmail.com>
2023-03-20 11:44:38 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,9 +1,1 @@
-def solution(array, n):
-    array.append(n)
-    array.sort()
-    i=array.index(n)
-    try: a=array[i-1] 
-    except: return array[i+1]
-    try: b=array[i+1] 
-    except: return array[i-1]
-    return a if n-a<=b-n else b
+solution=lambda a,n:sorted(a,key=lambda x:(abs(x-n),x))[0]
