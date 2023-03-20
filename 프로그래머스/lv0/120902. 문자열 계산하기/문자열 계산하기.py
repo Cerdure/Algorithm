22b5aec6a851fbfcf7221fa218b537c158009d15
Wonseok Choi <cerdure@gmail.com>
2023-03-20 17:54:16 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,7 +1,1 @@
-def solution(s):
-    s=s.split(' ')
-    x=int(s[0])
-    for i,n in enumerate(s):
-        if not n.isdigit():
-            x+=int(s[i+1])*(2*(n=='+')-1)
-    return x
+solution=lambda s:sum(map(int,s.replace('- ','+ -').split(' + ')))
