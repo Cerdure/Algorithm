3880c5d769327dc3cadff04c7bb27640a7b63972
Wonseok Choi <cerdure@gmail.com>
2023-03-20 18:10:50 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -1,6 +1,5 @@
-def solution(b, s):
-    def f(n):
-        i=1
-        while n:i*=n;n-=1
-        return i
-    return f(b)/(f(b-s)*f(s))
+solution=lambda b,s:f(b)/(f(b-s)*f(s))    
+def f(n):
+    i=1
+    while n:i*=n;n-=1
+    return i
