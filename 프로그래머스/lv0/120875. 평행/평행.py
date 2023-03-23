222571212193c2e4ef153b374c4c9184e894ffd7
Wonseok Choi <cerdure@gmail.com>
2023-03-23 21:30:45 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,5 +1,6 @@
 def solution(d):
     d=sorted(d,key=lambda x:x[0]+x[1])
-    c1=abs(d[0][0]-d[1][0])/abs(d[0][1]-d[1][1])==abs(d[2][0]-d[3][0])/abs(d[2][1]-d[3][1])
-    c2=abs(d[0][0]-d[2][0])/abs(d[0][1]-d[2][1])==abs(d[1][0]-d[3][0])/abs(d[1][1]-d[3][1])
+    x1,y1,x2,y2,x3,y3,x4,y4=sum(d,[])
+    c1=abs((x1-x2)/(y1-y2))==abs((x3-x4)/(y3-y4))
+    c2=abs((x1-x3)/(y1-y3))==abs((x2-x4)/(y2-y4))
     return (c1 | c2)*1
