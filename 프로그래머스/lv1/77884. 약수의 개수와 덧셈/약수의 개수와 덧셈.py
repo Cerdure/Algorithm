fa55a0011aeacefeb83cdf702bcf935b5654bff6
Wonseok Choi <cerdure@gmail.com>
2023-03-24 20:52:42 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,3 +1,1 @@
-solution=lambda l,r:sum([i*d(i) for i in range(l,r+1)])
-def d(n):
-    return (len([i for i in range(1,n//2+1) if i!=n and n%i==0])%2!=0)*2-1
+solution=lambda l,r:sum([i*((int(i**0.5)!=i**0.5)*2-1) for i in range(l,r+1)])
