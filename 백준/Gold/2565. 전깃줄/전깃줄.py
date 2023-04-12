a28d6c012693cfb46054e2f72aac9d9756e87b3c
Wonseok Choi <cerdure@gmail.com>
2023-04-12 15:18:18 +0900

    [Gold V] Title: 전깃줄, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,7 +1,3 @@
-n,*l=open(0)
-l=sorted([list(map(int,i.split())) for i in l],key=lambda x:x[0])
-d=[1]*(n:=int(n))
-for i in range(n):
-  for j in range(i):
-    if l[i][1]>l[j][1] and d[i]<=d[j]:d[i]=d[j]+1 
-print(n-max(d))
+n,*l=open(0);d=[0]*501
+for _,i in sorted([*map(int,i.split())] for i in l):d[i]=max(d[:i])+1
+print(int(n)-max(d))
