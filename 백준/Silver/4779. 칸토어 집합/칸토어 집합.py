338d8e08dbb26f257958d6ef63a3246f1d30404c
Wonseok Choi <cerdure@gmail.com>
2023-04-12 11:33:19 +0900

    [Silver III] Title: 칸토어 집합, Time: 48 ms, Memory: 32292 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,9 +1,1 @@
-def c(s,e,l):
-  if s==e:l[s]='-'
-  else:
-    d=(e-s+1)//3-1
-    c(s,s+d,l);c(e-d,e,l)
-for i in open(0):
-  n=3**int(i)
-  c(0,n-1,l:=[' ']*n)
-  print(''.join(l))
+for i in open(0):print((c:=lambda i:c(i-1)+' '*3**(i-1)+c(i-1) if i else '-')(int(i)))
