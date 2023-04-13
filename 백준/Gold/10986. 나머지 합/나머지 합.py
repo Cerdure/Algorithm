6bede6d9e2de7e16c1b6f32727d91da5e8d363c7
Wonseok Choi <cerdure@gmail.com>
2023-04-13 12:05:08 +0900

    [Gold III] Title: 나머지 합, Time: 500 ms, Memory: 149312 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -1,5 +1,4 @@
-m,a=open(0)
-n,m,a=*map(int,m.split()),[*map(int,a.split())]
+n,m,*a=map(int,open(0).read().split())
 p,t,c=[1]+[0]*(m-1),0,0
 for i in range(n):p[(t:=t+a[i])%m]+=1
 for i in p:c+=i*(i-1)//2
